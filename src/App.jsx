cb863c8cf5a26cce9ccb1c5c6323d407af46d0be
Sean Scanlon <sean@sbrcompliance.co.uk>
2026-05-11 21:57:00 +0100

    Fix header logo tick

--- src/App.jsx
+++ src/App.jsx
@@ -26,10 +26,9 @@ export default function BRMonitorLanding() {
           <div className="px-6 h-24 flex items-center justify-between">
             <div className="flex items-center gap-4">
               <div className="relative flex h-11 w-11 items-center justify-center rounded-xl border border-[#c8dde8] bg-[#2a8fa0]">
-                <div className="relative h-5 w-3">
-                  <div className="absolute left-0 bottom-0 h-2 w-[2px] rotate-[-45deg] rounded-full bg-white" />
-                  <div className="absolute right-0 top-0 h-5 w-[2px] rotate-[45deg] rounded-full bg-white" />
-                </div>
+               <svg viewBox="0 0 100 100" className="h-6 w-6" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <polyline points="28,50 43,65 72,35" stroke="white" strokeWidth="9" strokeLinecap="round" strokeLinejoin="round"/>
+</svg>
               </div>
               <div>
                 <div className="text-2xl leading-none font-semibold tracking-[-0.02em] text-[#1a2332]">
